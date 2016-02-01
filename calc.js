2e315d6e3cd8239f140f95c55c4ffc54d0f8d285
martti naaber <martti.naaber@gmail.com>
2016-02-01 12:28:49 +0200

    clock

--- calc.js
+++ calc.js
@@ -1,1 +1,23 @@
-asd
+
+//käivitub ainult siis. kui terve dokument on laetud
+window.onload = function(){
+
+  //saan kätte input väljad
+
+  var cm = document.getElementById('cm');
+  var inch = document.getElementById('inch');
+
+  //lisan nuppudele kuularid
+  document.getElementById('to_inch').addEventListener('click', convertToInch);
+  document.getElementById('to_cm').addEventListener('click', function(){
+    cm.value = inch.value * 2.54;
+
+  });
+};
+
+function convertToInch(){
+
+  //arvutan ümber tollideks ja lisan väärtuse tollid lahtrisse
+  inch.value = cm.value/2.54;
+
+}
