2e4034a91907be4d1dd1b430fec41b42985f18dd
martti naaber <martti.naaber@gmail.com>
2016-02-01 13:08:16 +0200

    clock is ready

--- clock.js
+++ clock.js
@@ -1,1 +1,38 @@
-asd
+window.onload = function(){
+
+  var clock = document.getElementById("clock");
+
+  writeDate();
+
+  window.setInterval(function(){
+
+    writeDate();
+
+  }, 1000); //millisekundid, 1000ms = 1s
+
+
+};
+
+function writeDate(){
+
+  var today = new Date();
+
+  var hours = today.getHours();
+  var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+
+  clock.innerHTML = setZeroBefore(hours) + ':' + setZeroBefore(minutes) + ':' + setZeroBefore(seconds);
+
+}
+
+//lisab nulli, kui arv on kümnest väiksem
+function setZeroBefore(number){
+
+  if(number <10){
+    number ='0' + number;
+
+  }
+
+  return number;
+
+}
